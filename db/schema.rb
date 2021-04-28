5203e51a4ba2e128056765c9319580040fb55293
Solyannik <solyannikolga@gmail.com>
2021-04-28 20:14:23 +0200

    add online channel which show online users

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_082920) do
+ActiveRecord::Schema.define(version: 20210428180914) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
@@ -32,8 +32,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_082920) do
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "online", default: false
   end
 
-  add_foreign_key "messages", "rooms"
-  add_foreign_key "messages", "users"
 end
